fda5d73114a0de78d4e89d355582af229f1e7be8
Jeremybbl <jeremy.boubal@gmail.com>
2020-07-05 22:38:17 +0200

    migrations fixed

--- migrations/Version20200629165348.php
+++ migrations/Version20200629165348.php
@@ -23,8 +23,8 @@ final class Version20200629165348 extends AbstractMigration
         $this->addSql('CREATE TABLE user_report (id INT AUTO_INCREMENT NOT NULL, reporter_id INT NOT NULL, reportee_id INT NOT NULL, INDEX IDX_A17D6CB9E1CFE6F5 (reporter_id), INDEX IDX_A17D6CB92C0189D3 (reportee_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE user_report ADD CONSTRAINT FK_A17D6CB9E1CFE6F5 FOREIGN KEY (reporter_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE user_report ADD CONSTRAINT FK_A17D6CB92C0189D3 FOREIGN KEY (reportee_id) REFERENCES user (id)');
-        $this->addSql('ALTER TABLE announcement_report ADD is_confirmed TINYINT(1) NOT NULL, DROP reason');
-        $this->addSql('ALTER TABLE user_report ADD is_confirmed TINYINT(1) NOT NULL, DROP reason');
+        $this->addSql('ALTER TABLE announcement_report ADD is_confirmed TINYINT(1) NOT NULL');
+        $this->addSql('ALTER TABLE user_report ADD is_confirmed TINYINT(1) NOT NULL');
         $this->addSql('ALTER TABLE announcement CHANGE status status TINYINT(1) NOT NULL');
         $this->addSql('ALTER TABLE user CHANGE status status TINYINT(1) NOT NULL');
         
